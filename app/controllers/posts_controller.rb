8a064bf02e896b69a750e509def1e3016302023a
Alexander Baas <ajmbaas@outlook.com>
2017-12-05 20:14:23 +0100

    create view and migrate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+
+def index
+  @posts = Post.all
+end
+
+def show
+  @post = Post.find(post_params)
+end
+
+def new
+end
+
+def create
+end
+
+def destroy
+end
+
+
+private
+
+  def post_params
+  end
+
 end
